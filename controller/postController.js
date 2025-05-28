d703f1713218c5763d9c5b612fc7d24038b69191
AgostaSalvatore <agosta.boolean@gmail.com>
2025-05-28 14:59:50 +0200

    [UPD] M3 completed

--- controller/postController.js
+++ controller/postController.js
@@ -7,7 +7,7 @@ const index = (req, res) => {
     connection.query(sql, (err, results) => {
         if (err) {
             console.log(err);
-            res.status(500).json({ error: 'Error during the reading of posts' });
+            res.status(500).json({ error: 'Error during the reading of posts' + err });
         } else {
             res.json(results);
         }
@@ -20,7 +20,16 @@ const show = (req, res) => {
 }
 
 const destroy = (req, res) => {
+    //query
+    const sql = 'DELETE FROM posts WHERE id = ?'
+    connection.query(sql, [req.params.id], (err, results) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ error: 'Error during the deletion of posts' + err });
+        }
 
+        res.sendStatus(204)
+    })
 }
 
 module.exports = {
