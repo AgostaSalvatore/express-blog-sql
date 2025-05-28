772a54f874f7643b89cbbb92551ae769e0be1fca
AgostaSalvatore <agosta.boolean@gmail.com>
2025-05-28 14:20:29 +0200

    feat: add middleware, static assets and root route handler to Express app

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//middleware
+app.use(express.json());
+
+//asset statici
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    console.log('ciao');
+    res.send('ciao')
+})
 //listen
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
